a2e4a2296efea58b18bfde76f2395f2f9242bf0c
chenchuk77 <chenchuk@gmail.com>
2023-09-06 11:10:16 +0300

    1.1.1: adding deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-ssh -i /opt/lms/keys/dev_access_key.pem ubuntu@freedomgpt "cd gamesbot && git pull && ./build-local-image.sh && ./stop-gamesbot && start-gamesbot.sh"
+ssh -i /opt/lms/keys/dev_access_key.pem ubuntu@freedomgpt "cd gamesbot && git pull && ./build-local-image.sh && ./stop-gamesbot.sh && start-gamesbot.sh"
--- stop-gamesbot.sh
+++ stop-gamesbot.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # get version from Dockerfile
-GAMESBOT_VERSION=$(grep 'GAMESBOT_VERSION' Dockerfile | cut -d "=" -f2)
+GAMESBOT_VERSION=$(docker ps | grep gamesbot | awk '{ print $2}' | cut -d ":" -f2)
 
 echo "stopping gamesbot v${GAMESBOT_VERSION} ..."
-docker stop gamesbot || true > /dev/null 2>&1
-
-#!/bin/bash
+docker stop gamesbot > /dev/null 2>&1 || true
+echo "gamesbot stopped."
